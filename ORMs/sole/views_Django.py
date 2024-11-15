954e4fdb0a3dd185537293a133e0d4443a61924d
Mverseau <mverseau@gmail.com>
2024-11-15 14:10:22 +0300

    Commit 15/11/2024 refactor

---
+++ ORMs/sole/views_Django.py
@@ -0,0 +1,143 @@
+import random
+import time
+from django.db.models import Count
+from django.db import connection
+from .models import *
+from .utils import ps_utils
+
+
+def dj_collect_data(data: dict, perf):
+    rez = perf.query_get_record_found(True)
+    for i in range(len(rez)):
+        data['SELECT buyer50 FROM Buyer'][0][i] += rez[i]
+
+    rez = perf.query_get_record_found(False)
+    for i in range(len(rez)):
+        data['SELECT buyer101 FROM Buyer'][0][i] += rez[i]
+
+    rez = perf.group_by()
+    for i in range(len(rez)):
+        data['SELECT age FROM Buyer GROUP BY age'][0][i] += rez[i]
+
+    rez = perf.sorting()
+    for i in range(len(rez)):
+        data['SELECT * FROM Buyer ORDER BY balance'][0][i] += rez[i]
+
+    rez = perf.count_elems()
+    for i in range(len(rez)):
+        data['SELECT COUNT(age) FROM Buyer WHERE age=?'][0][i] += rez[i]
+
+    rez = perf.joining()
+    for i in range(len(rez)):
+        data['SELECT * FROM Buyer\nJOIN Game ON Game.buyer=Buyer.name'][0][i] += rez[i]
+
+    rez = perf.add_record()
+    for i in range(len(rez)):
+        data['INSERT INTO Buyer (name, balance, age) VALUES (test2,1000, 101)'][0][i] += rez[i]
+
+    rez = perf.update_records()
+    for i in range(len(rez)):
+        data['UPDATE Buyer SET name ="TEST" WHERE name = "test2"'][0][i] += rez[i]
+
+    rez = perf.del_all_data()
+    for i in range(len(rez)):
+        data['DELETE FROM Buyer WHERE name like "% ?%"'][0][i] += rez[i]
+
+    return data
+
+
+class Dj():
+
+    def fill_in_buyers(self):
+        start = time.time()
+        for i in range(100):
+            Buyer.objects.create(
+                name=f'buyer{i}',
+                balance=random.randint(0, 100),
+                age=random.randint(1, 100)
+            )
+        end = time.time()
+        return f"{(end - start):.3f} сек."
+
+    def fill_in_games(self):
+
+        start = time.time()
+        for i in range(100):
+            Game.objects.create(
+                title=f'game{i}',
+                cost=random.randint(0, 100),
+                size=random.randint(1, 100),
+                description=f'description of game{i}',
+                age_limited=[True, False][random.randint(0, 1)],
+                buyer=Buyer.objects.get(id=random.randint(1, 100)).name
+            )
+        end = time.time()
+        return f"{(end - start):.3f} сек."
+
+    @ps_utils
+    def query_get_record_found(self, find_yes: bool):
+        start = time.time()
+        if find_yes:
+            Buyer.objects.get(name='buyer50')
+        else:
+            Buyer.objects.filter(name='buyer101')
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def group_by(self):
+        start = time.time()
+        Buyer.objects.values('age').annotate(total_posts=Count('age')).order_by('age')
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def sorting(self):
+        start = time.time()
+        Buyer.objects.all().order_by('balance')
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def count_elems(self):
+        start = time.time()
+        Buyer.objects.all().filter(age=random.randint(0, 100)).aggregate(Count('age'))
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def add_record(self):
+        start = time.time()
+        new_record = Buyer.objects.create(name="test DJ", balance=1000, age=101)
+        new_record.save()
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def del_all_data(self):
+        start = time.time()
+        Buyer.objects.filter(name__contains='DJ').delete()
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def joining(self):
+        start = time.time()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                    Select sole_game.title, sole_buyer.name from sole_game JOIN sole_buyer ON
+                     sole_game.buyer = sole_buyer.name
+                """)
+
+        end = time.time()
+        return end - start
+
+    @ps_utils
+    def update_records(self):
+        start = time.time()
+        query_for_filter = Buyer.objects.filter(name='test DJ')
+        query_for_filter.update(name=('test DJ') + ' Updated')
+        end = time.time()
+        return end - start
+
+perf=Dj()
--- ORMs/sole/views_SQLA.py
+++ ORMs/sole/views_SQLA.py
@@ -5,115 +5,120 @@ from sqlalchemy import create_engine
 from .models_SQLAlchemy import *
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from .utils import ps_utils#,#file
+from .utils import ps_utils  # ,#file
 
 
-
-def sql_alchem(data: dict,i):
+def sql_alchem(data: dict, i):
     # file.write(f'iter {i}: ')
     engine = create_engine(r'sqlite:///db.sqlite3', echo=True)
     with Session(autoflush=False, bind=engine) as db:
-        rez = a_query_get_record_found(db, True)
+        rez = sqla.a_query_get_record_found(db, True)
         for i in range(len(rez)):
             data['SELECT buyer50 FROM Buyer'][1][i] += rez[i]
-        rez = a_query_get_record_found(db, False)
+        rez = sqla.a_query_get_record_found(db, False)
         for i in range(len(rez)):
             data['SELECT buyer101 FROM Buyer'][1][i] += rez[i]
-        rez = al_group_by(db)
+        rez = sqla.al_group_by(db)
         for i in range(len(rez)):
             data['SELECT age FROM Buyer GROUP BY age'][1][i] += rez[i]
-        rez = a_sort(db)
+        rez = sqla.a_sort(db)
         for i in range(len(rez)):
             data['SELECT * FROM Buyer ORDER BY balance'][1][i] += rez[i]
-        rez = a_filter(db)
+        rez = sqla.a_filter(db)
         for i in range(len(rez)):
             data['SELECT COUNT(age) FROM Buyer WHERE age=?'][1][i] += rez[i]
-        rez = a_join(db)
+        rez = sqla.a_join(db)
         for i in range(len(rez)):
             data['SELECT * FROM Buyer\nJOIN Game ON Game.buyer=Buyer.name'][1][i] += rez[i]
-        rez = a_add_record(db)
+        rez = sqla.a_add_record(db)
         for i in range(len(rez)):
             data['INSERT INTO Buyer (name, balance, age) VALUES (test2,1000, 101)'][1][i] += rez[i]
-        rez = a_update_records(db)
+        rez = sqla.a_update_records(db)
         for i in range(len(rez)):
             data['UPDATE Buyer SET name ="TEST" WHERE name = "test2"'][1][i] += rez[i]
-        rez=a_delete(db)
+        rez = sqla.a_delete(db)
         for i in range(len(rez)):
             data['DELETE FROM Buyer WHERE name like "% ?%"'][1][i] += rez[i]
 
     db.close()
     return data
 
-@ps_utils
-def a_query_get_record_found(db: Session, find_yes: bool):
-    start = time.time()
-    if find_yes:
-        db.query(A_Buyer).filter(A_Buyer.name == 'buyer50').first()
-    else:
-        db.query(A_Buyer).filter(A_Buyer.name == 'buyer101').first()
 
-    end = time.time()
-    return end - start
+class SQLA():
+    @ps_utils
+    def a_query_get_record_found(self, db: Session, find_yes: bool):
+        start = time.time()
+        if find_yes:
+            db.query(A_Buyer).filter(A_Buyer.name == 'buyer50').first()
+        else:
+            db.query(A_Buyer).filter(A_Buyer.name == 'buyer101').first()
 
-@ps_utils
-def al_group_by(db: Session):
-    start = time.time()
-    db.query(A_Buyer.age, func.count(A_Buyer.age)).group_by(A_Buyer.age)
+        end = time.time()
+        return end - start
 
-    end = time.time()
-    return end - start
+    @ps_utils
+    def al_group_by(self, db: Session):
+        start = time.time()
+        db.query(A_Buyer.age, func.count(A_Buyer.age)).group_by(A_Buyer.age)
 
-@ps_utils
-def a_sort(db: Session):
-    start = time.time()
-    db.query(A_Buyer).order_by(A_Buyer.balance).all()
-    end = time.time()
-    return end - start
+        end = time.time()
+        return end - start
 
-@ps_utils
-def a_filter(db: Session):
-    start = time.time()
-    db.query(A_Buyer, func.count(A_Buyer.age)).filter(A_Buyer.age == random.randint(0, 100)).all()
-    end = time.time()
-    return end - start
+    @ps_utils
+    def a_sort(self, db: Session):
+        start = time.time()
+        db.query(A_Buyer).order_by(A_Buyer.balance).all()
+        end = time.time()
+        return end - start
 
-@ps_utils
-def a_join(db: Session):
-    start = time.time()
-    db.query(A_Buyer).filter(A_Buyer.name == A_Game.buyer).all()
-    end = time.time()
-    # file.write(f'{end-start=}\n')
-    return end - start
+    @ps_utils
+    def a_filter(self, db: Session):
+        start = time.time()
+        db.query(A_Buyer, func.count(A_Buyer.age)).filter(A_Buyer.age == random.randint(0, 100)).all()
+        end = time.time()
+        return end - start
 
-@ps_utils
-def a_add_record(db: Session):
-    start = time.time()
-    new_record = A_Buyer(
-        name="test A",
-        balance=1000,
-        age=101,
-    )
-    db.add(new_record)
-    db.commit()
-    end = time.time()
-    return end - start
+    @ps_utils
+    def a_join(self, db: Session):
+        start = time.time()
+        db.query(A_Buyer).filter(A_Buyer.name == A_Game.buyer).all()
+        end = time.time()
+        # file.write(f'{end-start=}\n')
+        return end - start
 
-@ps_utils
-def a_update_records(db: Session):
-    start = time.time()
+    @ps_utils
+    def a_add_record(self, db: Session):
+        start = time.time()
+        new_record = A_Buyer(
+            name="test A",
+            balance=1000,
+            age=101,
+        )
+        db.add(new_record)
+        db.commit()
+        end = time.time()
+        return end - start
 
-    db.query(A_Buyer).filter(A_Buyer.name == 'test A').update({'name': 'TEST A'},
-                                                             synchronize_session='fetch')
-    db.commit()
+    @ps_utils
+    def a_update_records(self, db: Session):
+        start = time.time()
 
-    end = time.time()
-    return end - start
-@ps_utils
-def a_delete(db: Session):
-    start = time.time()
+        db.query(A_Buyer).filter(A_Buyer.name == 'test A').update({'name': 'TEST A'},
+                                                                  synchronize_session='fetch')
+        db.commit()
 
-    db.query(A_Buyer).filter(A_Buyer.name.contains('_A')).delete()
-    db.commit()
+        end = time.time()
+        return end - start
 
-    end = time.time()
-    return end - start
+    @ps_utils
+    def a_delete(self, db: Session):
+        start = time.time()
+
+        db.query(A_Buyer).filter(A_Buyer.name.contains('_A')).delete()
+        db.commit()
+
+        end = time.time()
+        return end - start
+
+
+sqla = SQLA()
